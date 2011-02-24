f16e800db6b814baaf82bc7d11dab4956f9cce8d
Zeke Sikelianos <zeke@sikelianos.com>
2011-02-23 17:25:07 -0800

    API key must be set as an environment variable when running 'rake spec'.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,12 @@ VCR.config do |config|
   config.stub_with :webmock # or :fakeweb
 end
 
+unless ENV['KEY']
+  puts "\nOn Noes! You gotta include your API key when running the specs, like so:\n\nKEY=12345 rake spec\n\n"
+  exit
+end
+
 Wordnik.configure do |config|
-  config.api_key = "12345"
+  config.api_key =  ENV['KEY']
+  config.base_uri = "beta.wordnik.com/v4"
 end
